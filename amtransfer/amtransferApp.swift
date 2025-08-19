af1bd426723235fa0fb32d5aa447e3bed1afbf45
escofresco <erinakarice@gmail.com>
2025-08-19 08:04:17 -0700

    rearrange it for xcode

---
+++ amtransfer/amtransferApp.swift
@@ -0,0 +1,17 @@
+//
+//  amtransferApp.swift
+//  amtransfer
+//
+//  Created by Erin Akarice on 8/19/25.
+//
+
+import SwiftUI
+
+@main
+struct amtransferApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ amtransferTests/amtransferTests.swift
@@ -0,0 +1,17 @@
+//
+//  amtransferTests.swift
+//  amtransferTests
+//
+//  Created by Erin Akarice on 8/19/25.
+//
+
+import Testing
+@testable import amtransfer
+
+struct amtransferTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
