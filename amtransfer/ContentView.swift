01370841e680fc5c0dee983b17fa6c0a9e24710a
escofresco <erinakarice@gmail.com>
2025-08-23 06:02:08 -0700

    make contentview taller

--- amtransfer/ContentView.swift
+++ amtransfer/ContentView.swift
@@ -26,6 +26,6 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
             .previewLayout(.sizeThatFits)
-            .frame(minWidth: 400, minHeight: 300)
+            .frame(minWidth: 400, minHeight: 500)
     }
 }
