0897373626b003ef898d4edf42027ff7fb381095
erin <erinakarice@gmail.com>
2025-09-15 04:54:17 -0700

    Implement playlist creation request

--- amtransfer/AM/Models/PlaylistCreationRequest.swift
+++ amtransfer/AM/Models/PlaylistCreationRequest.swift
@@ -1,4 +1,5 @@
 import MusicKit
+import Foundation
 
 /// Minimal wrapper to create a playlist in the user's Apple Music library.
 ///
@@ -13,8 +14,30 @@ struct PlaylistCreationRequest {
     /// Tracks to include in the new playlist.
     var trackIDs: [MusicItemID]
 
-    /// Sends the creation request. Currently a no-op placeholder.
+    /// Sends the playlist creation request to Apple Music.
+    ///
+    /// This implementation uses ``MusicDataRequest`` to call the
+    /// `me/library/playlists` endpoint and create the playlist with the
+    /// supplied tracks.
     func response() async throws {
-        // TODO: Use MusicKit's playlist creation endpoint when available.
+        let url = URL(string: "https://api.music.apple.com/v1/me/library/playlists")!
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "POST"
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        let body: [String: Any] = [
+            "attributes": ["name": name],
+            "relationships": [
+                "tracks": [
+                    "data": trackIDs.map { ["id": $0.rawValue, "type": "songs"] }
+                ]
+            ]
+        ]
+
+        urlRequest.httpBody = try JSONSerialization.data(withJSONObject: body)
+
+        var request = MusicDataRequest(urlRequest: urlRequest)
+        request.requiresMusicUserToken = true
+        _ = try await request.response()
     }
 }
